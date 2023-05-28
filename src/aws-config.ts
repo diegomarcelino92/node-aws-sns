d2d67de8f8c6e2cc6750661f46355d8a78da5c80
diegomarcelino92 <ddmarcelino2014@gmail.com>
2023-05-28 20:34:26 -0300

    fix: using aws update method for config

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -1,9 +1,11 @@
 import aws from "aws-sdk";
 
-aws.config.region = "us-east-1";
-aws.config.credentials = {
-  accessKeyId: "", // put here your Access Key
-  secretAccessKey: "", // put here your Secret Access Key
-};
+aws.config.update({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: "", // put here your Access Key
+    secretAccessKey: "", // put here your Secret Access Key
+  },
+});
 
 export const sns = new aws.SNS();
